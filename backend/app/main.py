edda72cdfa772a20ae55f310f7307065ca396e09
Ilie1985 <ilie.marian32@yahoo.com>
2026-06-20 13:38:44 +0100

    Refactor CORS middleware configuration: replace wildcard origins with specific allowed origins for enhanced security.

--- backend/app/main.py
+++ backend/app/main.py
@@ -79,9 +79,15 @@ app = FastAPI(
 )
 
 
+origins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "ai-banking-project-fapap949q-ilie1985s-projects.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
